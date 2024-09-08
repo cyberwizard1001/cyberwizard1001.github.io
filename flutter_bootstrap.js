f3fd573f7468653efeeb4e335ac4053590a465d2
cyberwizard1001 <cyberwizard1001@users.noreply.github.com>
2024-09-08 04:50:13 +0000

    Deploy Flutter web app from cyberwizard1001/flutter_portfolio@ac2e139e2f2d5fc3e02c90359c9b250c54d34832

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "569981845"
+    serviceWorkerVersion: "3206535629"
   }
 });
